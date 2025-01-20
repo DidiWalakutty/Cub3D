4a96a9d3447d5b15bf24816a7ee404413b52701f
Didi Walakuttij <diwalaku@f1r6s11.codam.nl>
2025-01-20 14:29:09 +0100

    added header

---
+++ include/cub3d.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   cub3d.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/01/20 14:25:31 by diwalaku      #+#    #+#                 */
+/*   Updated: 2025/01/20 14:28:51 by diwalaku      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <math.h>
+# include <errno.h>
+# include <string.h>
+# include <stdbool.h>
+
+# include "MLX42/MLX42.h"
+# include "libft.h"
+
+#endif
