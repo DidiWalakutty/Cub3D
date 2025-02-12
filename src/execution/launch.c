a62e9bf730064305850f2db25ccc3b4c8f8257ab
Didi Walakutty <91764214+DidiWalakutty@users.noreply.github.com>
2025-02-12 19:13:15 +0100

    Major Update

--- src/execution/launch.c
+++ src/execution/launch.c
@@ -6,48 +6,48 @@
 /*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/23 19:29:54 by diwalaku      #+#    #+#                 */
-/*   Updated: 2025/02/11 14:29:09 by diwalaku      ########   odam.nl         */
+/*   Updated: 2025/01/29 15:09:40 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// Do we need to reset the image/screen?
-static void	raycaster(void *data)
+void	draw_environment(t_cub3d *cub3d)
 {
-	t_cub3d		*cub3d;
-	t_render	*render;
-	int			y;
+	int	x;
+	int	y;
 
-	cub3d = data;
-	render = cub3d->render;
-	ft_memset(cub3d->scene->pixels, 0xFF000000, S_WIDTH * S_HEIGTH * sizeof(uint32_t));
 	y = 0;
-	while (y < S_WIDTH)
+	while (y < S_HEIGTH)
 	{
-		create_ray(cub3d, render, y);
-		y++;
+		x = 0;
+		while (x < S_WIDTH)
+		{
+			
+		}
 	}
 }
 
-void	test()
-{
-
-}
-
 // TODO:
+// set colors and walls
 // if time: minimap
-// raycaster/render
+// raycaster
 // MLX hooks and loop
 void	run_cub3d(t_cub3d *cub3d)
 {
-	cub3d->mlx = mlx_init(S_WIDTH, S_HEIGTH, "Cub3D", false); // creates window
+	mlx_set_setting(MLX_STRETCH_IMAGE, 1); 		// does resizing of window
+	cub3d->mlx = mlx_init(S_WIDTH, S_HEIGTH, "Cub3D", true);	// creates window
 	if (!cub3d->mlx)
 		end_game(cub3d, "Couldn't init MLX window");
-	init_settings(cub3d);
+	set_mlx_and_variables(cub3d);				// sets floor + ceiling
+	// TODO:
+	if (load_wall_textures(cub3d) == false)
+		end_game(cub3d, "Couldn't load textures");
+	draw_environment(cub3d);
+	// raycaster
 	// if time left: mini_map
-	mlx_loop_hook(cub3d->mlx, keys, cub3d);
-	mlx_loop_hook(cub3d->mlx, &raycaster, (void*)cub3d);
-	// mlx_loop_hook(cub3d->mlx, test, cub3d); // As long as raycaster isn't functioning, use this to test background
-	mlx_loop(cub3d->mlx);
+	// mlx hooks
+	// mlx_loop
+	
 }
+
--- src/set_up/set_up.c
+++ src/set_up/set_up.c
@@ -6,67 +6,108 @@
 /*   By: diwalaku <diwalaku@codam.student.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/24 13:49:41 by diwalaku      #+#    #+#                 */
-/*   Updated: 2025/02/11 13:54:59 by diwalaku      ########   odam.nl         */
+/*   Updated: 2025/01/29 15:47:33 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include <math.h>
 
-static void	initiate_mlx_images(t_cub3d *cub3d)
+static void	set_n_and_s(t_player *player)
 {
-	cub3d->scene = mlx_new_image(cub3d->mlx, S_WIDTH, S_HEIGTH);
-	cub3d->floor_and_ceiling = mlx_new_image(cub3d->mlx, \
-										S_WIDTH, S_HEIGTH);
-	if (!cub3d->scene || !cub3d->floor_and_ceiling)
-		end_game(cub3d, "Allocation error for image space");
-	if (mlx_image_to_window(cub3d->mlx, cub3d->floor_and_ceiling, \
-															0, 0) == -1)
-		end_game(cub3d, "Couldn't output image to window");
-	if (mlx_image_to_window(cub3d->mlx, cub3d->scene, 0, 0) == -1)
-		end_game(cub3d, "Couldn't output image to window");
-	fill_background(cub3d);
-	if (load_wall_textures(cub3d) == false)
-		end_game(cub3d, "Couldn't load textures");
+	if (player->orientation == 'N')
+	{
+		player->direction.x = 0;
+		player->direction.y = -1;
+		player->plane.x = -0.66;
+		player->plane.y = 0;
+	}
+	else
+	{
+		player->direction.x = 0;
+		player->direction.y = 1;
+		player->plane.x = 0.66;
+		player->plane.y = 0;
+	}
+}
+
+// Map's origin (0, 0) is top left corner.
+// Y is vertical, X is horizontal
+// the plane is the camera plane that represents the screen and it always runs
+// perpendicular to our direction
+static void	set_direction_and_plane(t_player *player)
+{
+	if (player->orientation == 'N' || player->orientation == 'S')
+		set_n_and_s(player);
+	else if (player->orientation == 'E')
+	{
+		player->direction.x = 1;
+		player->direction.y = 0;
+		player->plane.x = 0;
+		player->plane.y = -0.66;
+	}
+	else if (player->orientation == 'W')
+	{
+		player->direction.x = -1;
+		player->direction.y = 0;
+		player->plane.x = 0;
+		player->plane.y = 0.66;
+	}
+	player->position.x = (double)player->x;
+	player->position.y = (double)player->y;
+}
+
+// Need to check which variables we need in here.
+// Probably angle, FOV etc as well.
+// Need map info for direction.
+static void	set_variables(t_cub3d *cub3d)
+{
+	set_direction_and_plane(cub3d->player); // map info: found letter for direction
+	cub3d->player->fov = (FOV * PI) / 180;	// FOV in radians for sin(), cos() and tan().
+	// cub3d->player.rotation = 
+}
+
+// scene is pointer to the pixel array.
+static void	fill_background(void *scene, int ceiling_color, int floor_color, \
+			size_t total_pixels)
+{
+	size_t	i;
+	int		*image;
+
+	image = (int *)scene;
+	i = 0;
+	while (i < S_UPPER_HALF)
+	{
+		image[i] = ceiling_color;
+		i++;
+	}
+	while (i < total_pixels)
+	{
+		image[i] = floor_color;
+		i++;
+	}
 }
 
 // mlx_new_image: Creates and allocates a new image buffer.
 // mlx_image_to_window: Draws a new instance of an image, 
 // it will then share the same pixel buffer as the image. Returns -1 if failed
-void	init_settings(t_cub3d *cub3d)
+// Choose colors for ceiling and floor!
+void	set_mlx_and_variables(t_cub3d *cub3d)
 {
-	if (!alloc_execution_structs(cub3d))
-		end_game(cub3d, "Couldn't allocate structs");
-	initiate_mlx_images(cub3d);
-	cub3d->render = set_variables(cub3d);
-	if (!cub3d->render)
-		end_game(cub3d, "Couldn't alloc render struct");
-}
+	cub3d->textures->floor_color = rgba_to_int(cub3d->input->ceiling_colors, 255);
+	cub3d->textures->ceiling_color = rgba_to_int(cub3d->input->floor_colors, 255);
 
-bool	load_wall_textures(t_cub3d *cub3d)
-{
-	cub3d->textures->north = mlx_load_png("textures/Space_N.png");
-	cub3d->textures->south = mlx_load_png("textures/Space_S.png");
-	cub3d->textures->east = mlx_load_png("textures/Space_E.png");
-	cub3d->textures->west = mlx_load_png("textures/Space_W.png");
-	if (!cub3d->textures->north || !cub3d->textures->south || \
-		!cub3d->textures->east || !cub3d->textures->west)
-		return (false);
-	return (true);
-}
-
-void	draw_calculations(t_render *ray, t_cub3d *cub3d)
-{
-	if (ray->wall_dist == 0)
-		ray->line.height = S_HEIGTH;
-	else
-		ray->line.height = (int)(S_HEIGTH / ray->wall_dist);
-	ray->line.start = (S_HEIGTH / 2) - (ray->line.height / 2);
-	if (ray->line.start < 0)
-		ray->line.start = 0;
-	ray->line.end = (S_HEIGTH / 2) + (ray->line.height / 2);
-	if (ray->line.end >= S_HEIGTH)
-		ray->line.end = S_HEIGTH - 1;
-	cub3d->textures->pix_step = 1.0 * cub3d->textures->wall_img->height / ray->line.height;
-	cub3d->textures->tex_pos = (ray->line.start - S_HEIGTH / 2 + ray->line.height) \
-								* cub3d->textures->pix_step;
+	cub3d->render.wall = mlx_new_image(cub3d->mlx, S_WIDTH, S_HEIGTH);
+	cub3d->render.floor_and_ceiling = mlx_new_image(cub3d->mlx, \
+													S_WIDTH, S_HEIGTH);
+	if (!cub3d->render.wall || !cub3d->render.floor_and_ceiling)
+		end_game(cub3d, "Allocation error for image space");
+	if (mlx_image_to_window(cub3d->mlx, cub3d->render.wall, 0, 0) == -1)
+		end_game(cub3d, "Couldn't output image to window");
+	if (mlx_image_to_window(cub3d->mlx, cub3d->render.floor_and_ceiling, 0, 0) == -1)
+		end_game(cub3d, "Couldn't output image to window");
+	fill_background(cub3d->render.floor_and_ceiling->pixels, \
+	cub3d->textures->ceiling_color, cub3d->textures->floor_color, \
+					S_UPPER_HALF + S_LOWER_HALF);
+	set_variables(cub3d);
 }
