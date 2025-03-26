78df122a152d62d1211da2e36f31582dec806a30
Y4ZK4Y <yk@yasamankarimi@gmail.com>
2025-03-26 12:57:03 +0100

    restore accidentally deleted fps-hook function

--- src/execution/time.c
+++ src/execution/time.c
@@ -1,16 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   time.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yasamankarimi <yasamankarimi@student.42    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/23 12:22:43 by yasamankari       #+#    #+#             */
-/*   Updated: 2025/03/23 12:23:57 by yasamankari      ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   time.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: yasamankarimi <yasamankarimi@student.42      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/03/23 12:22:43 by yasamankari   #+#    #+#                 */
+/*   Updated: 2025/03/26 12:55:24 by ykarimi       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	fps_hook(void *param)
+{
+	static uint32_t	fps;
+	t_cub3d			*meta;
+
+	meta = param;
+	if (!(meta->fps_timer.time_func))
+	{
+		fps = 0;
+		timer_init(&meta->fps_timer, mlx_get_time);
+	}
+	if (timer_delta(&meta->fps_timer) >= 1)
+	{
+		meta->fps = fps;
+		printf("FPS: [%u]\n", meta->fps);
+		timer_start(&meta->fps_timer);
+		fps = 0;
+	}
+	else
+		fps++;
+}
 
 t_timer	*timer_init(t_timer *t, double (*time_func)(void))
 {
